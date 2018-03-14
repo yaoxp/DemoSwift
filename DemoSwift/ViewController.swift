71c8bfaca962672d90fa7ac592f65bbcd795df3b
yaoxp <viq_xp@126.com>
2018-03-14 11:13:11 +0800

    Initial Commit

---
+++ DemoSwift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DemoSwift
+//
+//  Created by yaoxinpan on 2018/3/14.
+//  Copyright © 2018年 yaoxp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
