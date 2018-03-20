a1cbcb318a8e328c139a02c653a5a19ff4828684
yaoxp <viq_xp@126.com>
2018-03-20 20:35:49 +0800

    添加忘记提交的文件

---
+++ DemoSwift/Demo/PayTheBill/PayTheBillHeaderView.swift
@@ -0,0 +1,20 @@
+//
+//  PayTheBillHeaderView.swift
+//  DemoSwift
+//
+//  Created by yaoxinpan on 2018/3/19.
+//  Copyright © 2018年 yaoxp. All rights reserved.
+//
+
+import UIKit
+
+class PayTheBillHeaderView: UIView, NibLoadable {
+
+    @IBOutlet weak var iconImgView: UIImageView!
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    @IBOutlet weak var imgViewHeightAndWidth: NSLayoutConstraint!
+    
+    @IBOutlet weak var imgViewLeading: NSLayoutConstraint!
+}
